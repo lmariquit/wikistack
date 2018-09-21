93f05ee66fc2025c7e2a54c17f97a9970c940de8
Patrick Aloia <patrickalexanderaloia@gmail.com>
2018-09-21 14:22:31 -0400

    first push to github

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
 const morgan = require("morgan");
+
+//database
+const { db, Page, User } = require('./models');
+
+
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
+//DB Syncs
+const init = async () => {
+    await db.sync();
+    await Page.sync();
+    await User.sync();
+    
+    app.listen(3000, () => {
+        console.log('Server is up')
+    });
+}
+
+init();
+
 const layout = require('./views/layout.js');
 
 const bodyParser = require('body-parser');
@@ -13,4 +36,3 @@ app.get('/', (req, res, next) => {
     res.send(layout())
 })
 
-app.listen(3000);
